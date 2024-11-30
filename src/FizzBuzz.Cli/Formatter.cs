e5bbee8136ed7291b0b91311bbf56637765f0450
MaximVVolkov <maksim_volkov1@epam.com>
2024-11-30 18:59:46 +0300

    extracts formatter class static method

---
+++ src/FizzBuzz.Cli/Formatter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Cli
+{
+    public class Formatter
+    {
+        public static string FormatWithRules(int i1)
+        {
+            string? line;
+            if (i1 % 3 == 0 && i1 % 5 == 0)
+            {
+                var fizzbuzz = "FizzBuzz";
+                line = fizzbuzz;
+            }
+            else if (i1 % 3 == 0)
+            {
+                var fizz = "Fizz";
+                line = fizz;
+            }
+            else if (i1 % 5 == 0)
+            {
+                var buzz = "Buzz";
+                line = buzz;
+            }
+            else
+            {
+                line = i1.ToString();
+            }
+            string s = line;
+            return s;
+        }
+    }
+}
--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -1,27 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using FizzBuzz.Cli;
+
 for (var i = 1; i <= 100; i++)
 {
-    string? line;
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-        var fizzbuzz = "FizzBuzz";
-        line = fizzbuzz;
-    }
-    else if (i % 3 == 0)
-    {
-        var fizz = "Fizz";
-        line = fizz;
-    }
-    else if (i % 5 == 0)
-    {
-        var buzz = "Buzz";
-        line = buzz;
-    }
-    else
-    {
-        line = i.ToString();
-    }
-    string result = line;
+    var result = Formatter.FormatWithRules(i);
     Console.WriteLine(result);
-    
 }
+
