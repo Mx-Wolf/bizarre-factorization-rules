c037e7927ce92cd3d2d9e4676ca2e6bcfefc7145
MaximVVolkov <maksim_volkov1@epam.com>
2024-12-19 16:39:34 +0300

    for presentation readability

--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -5,13 +5,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder();
+
 builder.Services.AddSingleton<IDriver, Driver>();
 builder.Services.AddSingleton<IGenerator, Generator>();
 builder.Services.AddSingleton<IFormatter, Formatter>();
 builder.Services.AddSingleton<IRules, Rules>();
+builder.Services.AddSingleton<Action<string>>(Console.WriteLine);
+
 builder.Services.AddOptions<WordSettings>().Bind(builder.Configuration.GetSection(nameof(WordSettings)));
 builder.Services.AddOptions<GeneratorSettings>().Bind(builder.Configuration.GetSection(nameof(GeneratorSettings)));
-builder.Services.AddSingleton<Action<string>>(Console.WriteLine);
+
 builder.Services.AddHostedService<Service>();
 
 var app = builder.Build();
