9c858af8e134cdc49adc826577a62fd07983082b
MaximVVolkov <maksim_volkov1@epam.com>
2024-12-23 22:44:43 +0300

    demo random error

---
+++ src/FizzBuzz.Cli/CustomWriter.cs
@@ -0,0 +1,23 @@
+﻿namespace FizzBuzz.Cli
+{
+    class CustomWriter(TextWriter writer) : TextWriter
+    {
+        private static readonly Random rnd = new Random(23);
+        public override void WriteLine(string? value)
+        {
+            if (value == "FizzBuzz" && rnd.Next(100)<70)
+            {
+                throw new ArgumentException("Exception occurred: 'FizzBuzz' is not allowed.", nameof(value));
+            }
+
+            writer.WriteLine(value);
+        }
+
+        public override void WriteLine(int value)
+        {
+            writer.WriteLine(value);
+        }
+
+        public override System.Text.Encoding Encoding => writer.Encoding;
+    }
+}
