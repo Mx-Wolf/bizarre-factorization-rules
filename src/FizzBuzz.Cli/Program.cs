5ded9d272893564a997b1cab2431108e29c5e8a4
Maxim Volkov <mx_ruz_net@hotmail.com>
2024-12-24 16:27:08 +0300

    Drafts/step 01 init1 (#6)
    * demo random error
    
    * initial
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>

--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FizzBuzz.Cli;
+
+var originalOut = Console.Out;
+var newOut = new CustomWriter(originalOut);
+Console.SetOut(newOut);
+
 for (var i = 1; i <= 100; i++)
 {
     if (i % 3 == 0 && i % 5 == 0)
