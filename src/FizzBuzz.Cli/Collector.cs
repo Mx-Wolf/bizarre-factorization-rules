4e5dbe5f2a27852e9bd52d7d92ab6ddd15427d99
MaximVVolkov <maksim_volkov1@epam.com>
2024-12-27 11:01:59 +0300

    expression body

--- src/FizzBuzz.Cli/Collector.cs
+++ src/FizzBuzz.Cli/Collector.cs
@@ -2,8 +2,5 @@ namespace FizzBuzz.Cli;
 
 public class Collector(TextWriter writer) : ICollector
 {
-    public void Collect(string line)
-    {
-        writer.WriteLine(line);
-    }
+    public void Collect(string line) => writer.WriteLine(line);
 }
