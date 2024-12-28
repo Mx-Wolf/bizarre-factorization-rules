f6948a825cccccfa7962c0777461a5624ec32ff9
MaximVVolkov <maksim_volkov1@epam.com>
2024-12-28 11:21:05 +0300

    makes use of string.join

--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -1,14 +1,10 @@
-﻿Console.Write(
-    Enumerable
-        .Range(1, 100)
-        .Select(x => (x % 3, x % 5) switch
+﻿Console.WriteLine(string.Join(
+    Environment.NewLine,
+    Enumerable.Range(1, 100).Select(
+        x => (x % 3, x % 5) switch
         {
             (0, 0) => "FizzBuzz",
             (0, _) => "Fizz",
             (_, 0) => "Buzz",
             _ => x.ToString()
-        })
-        .Aggregate(
-            new StringBuilder(),
-            (a, b) => a.Append(b).Append(Environment.NewLine),
-            a => a.ToString()));
+        })));
