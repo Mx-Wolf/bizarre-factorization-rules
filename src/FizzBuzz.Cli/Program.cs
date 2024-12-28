b90478ec32564fb7c6d6053129b16147356d48e7
MaximVVolkov <maksim_volkov1@epam.com>
2024-12-28 10:33:57 +0300

    with result selector

--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -3,11 +3,12 @@
         .Range(1, 100)
         .Select(x => (x % 3, x % 5) switch
         {
-            (0, 0) => "FizzBuzz", 
-            (0, _) => "Fizz", 
-            (_, 0) => "Buzz", 
+            (0, 0) => "FizzBuzz",
+            (0, _) => "Fizz",
+            (_, 0) => "Buzz",
             _ => x.ToString()
         })
         .Aggregate(
-            new StringBuilder(), 
-            (a, b) => a.Append(b).Append(Environment.NewLine)));
+            new StringBuilder(),
+            (a, b) => a.Append(b).Append(Environment.NewLine),
+            a => a.ToString()));
