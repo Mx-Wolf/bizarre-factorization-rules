1f9a7498265720a881703689f212927e39d292cb
MaximVVolkov <maksim_volkov1@epam.com>
2024-12-30 16:19:14 +0300

    improve unit test for formatter

---
+++ tests/FizzBuzz.CliTests/DriverTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using FizzBuzz.Cli;
+using Moq;
+
+namespace FizzBuzz.CliTests
+{
+    public class DriverTests
+    {
+        private readonly Mock<IGenerator> generator = new();
+        private readonly Mock<IFormatter> formatter = new();
+        private readonly Mock<ICollector> collector = new();
+        private readonly Fixture fix = new();
+        [Theory]
+        [AutoData]
+        public void DriverUsesServices([Range(3,200)] int count)
+        {
+          
+            var sequence = fix.CreateMany<int>(count);
+            var line = fix.Create<string>();
+            generator.Setup(e => e.GetRange()).Returns(sequence);
+            formatter.Setup(e => e.Format(It.IsAny<int>())).Returns(line);
+
+            var sut = GetSut();
+
+            sut.Execute();
+
+            generator.Verify(e=>e.GetRange(),Times.Once);
+            formatter.Verify(e=>e.Format(It.IsAny<int>()),Times.Exactly(count));
+            collector.Verify(e=>e.Collect(It.IsAny<string>()),Times.Exactly(count));
+        }
+
+        private Driver GetSut()
+        {
+            return new Driver(
+                generator.Object,
+                formatter.Object,
+                collector.Object);
+        }
+    }
+}
