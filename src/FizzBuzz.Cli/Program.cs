f933d3ae2a357f4e88059fd74515e1034a96edf0
Maxim Volkov <mx_ruz_net@hotmail.com>
2024-12-30 18:41:02 +0300

    butifies (#25)
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>

--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -7,20 +7,15 @@ internal static class Program
 {
     public static void Main(string[] args)
     {
-        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true).Build();
-        var services = new ServiceCollection();
-        services.AddOptions<FormatterSettings>().Bind(configuration.GetSection(nameof(FormatterSettings)));
-        services.AddOptions<RulesSettings>().Bind(configuration.GetSection(nameof(RulesSettings)));
-        services.AddOptions<GeneratorSettings>().Bind(configuration.GetSection(nameof(GeneratorSettings)));
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true)
+            .Build();
 
-        services.AddSingleton<IRules, Rules>();
-        services.AddSingleton<IFormatter, Formatter>();
-        services.AddSingleton<IGenerator, Generator>();
-        services.AddSingleton<ICollector, Collector>();
-        services.AddSingleton<IDriver, Driver>();
-        
-        var serviceProvider = services.BuildServiceProvider();
-        var driver = serviceProvider.GetRequiredService<IDriver>();
+        var driver = new ServiceCollection()
+            .Bootstrap(configuration)
+            .BuildServiceProvider()
+            .GetRequiredService<IDriver>();
+
         driver.Execute();
     }
 }
