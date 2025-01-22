20ba0a5d3f54f6720ef6cf63463d1d01a125fe67
Maxim Volkov <mx_ruz_net@hotmail.com>
2025-01-22 19:08:47 +0300

    Drafts/s2 (#41)
    * Steps/step 01 (#8)
    
    * Drafts/step 01 initial (#2)
    
    * initial commit
    
    * makes cli program
    
    * adds the task description
    
    * adds the task description
    
    * Adds Naive Implementation :)
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * Drafts/step 01 init1 (#6)
    
    * demo random error
    
    * initial
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#7)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * Drafts/starting (#31)
    
    * Drafts/step 01 initial (#2)
    
    * initial commit
    
    * makes cli program
    
    * adds the task description
    
    * adds the task description
    
    * Adds Naive Implementation :)
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * Drafts/step 01 init1 (#6)
    
    * demo random error
    
    * initial
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#7)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#9)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#20)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * adds a bit of readme.md (#21)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * fixes formatting (#26)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * improves readme (#27)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * Drafts/m1 (#35)
    
    * Drafts/step 01 initial (#2)
    
    * initial commit
    
    * makes cli program
    
    * adds the task description
    
    * adds the task description
    
    * Adds Naive Implementation :)
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * Drafts/step 01 init1 (#6)
    
    * demo random error
    
    * initial
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#7)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#9)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial classroom implementation (#20)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * adds a bit of readme.md (#21)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * fixes formatting (#26)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * improves readme (#27)
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * updates the original task
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>
    
    * initial unit tests before refatoring
    
    ---------
    
    Co-authored-by: MaximVVolkov <maksim_volkov1@epam.com>

--- src/FizzBuzz.Cli/Program.cs
+++ src/FizzBuzz.Cli/Program.cs
@@ -1,19 +1,25 @@
-﻿for (var i = 1; i <= 100; i++)
+﻿public class Program
 {
-    if (i % 3 == 0 && i % 5 == 0)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("FizzBuzz");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.WriteLine("Fizz");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine("Buzz");
-    }
-    else
-    {
-        Console.WriteLine(i);
+        for (var i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
